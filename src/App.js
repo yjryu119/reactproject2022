269fe6022e59b6ea06bc165cf68c6d5664bbee07
yeojeongRyu <yjryu777@gmail.com>
2022-06-14 23:46:25 +0900

    test base name

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <BrowserRouter basename="/reactproject2022/">
+      <BrowserRouter>
         <ScrollTop />
         <NavBar keyword={keyword} setKeyword={setKeyword}></NavBar>
         <Routes>
